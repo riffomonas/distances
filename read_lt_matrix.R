aa72fa3f662e6d0fddf3f10b5408ad7c4029a0dc
Pat Schloss <pschloss@umich.edu>
2021-12-10 13:15:25 -0500

    Replace for loop with lapply

--- read_lt_matrix.R
+++ read_lt_matrix.R
@@ -1,22 +1,25 @@
-distances <- scan("mice.braycurtis.dist",
+file <- scan("mice_simple.braycurtis.dist",
                   what=character(),
                   quiet=TRUE,
-                  sep="\t")
+                  sep="\n")
 
-n_samples <- as.numeric(distances[1])
-distances <- distances[-1]
+n_samples <- as.numeric(file[1])
+file <- file[-1]
 
-dist_matrix <- matrix(0, nrow=n_samples, ncol=n_samples)
-samples <- rep("", n_samples)
+file_split <- strsplit(file, "\t")
 
-samples[1] <- distances[1]
-distances <- distances[-1]
-
-for(i in 2:n_samples){
-  samples[i] <- distances[1]
-  distances <- distances[-1]
-  
-  dist_matrix[i, 1:(i-1)] <- as.numeric(distances[1:(i-1)])
-  dist_matrix[1:(i-1), i] <- dist_matrix[i, 1:(i-1)]
-  distances <- distances[-(1:(i-1))]
+fill_in <- function(x, length){
+  c(x, rep("0", length - length(x)))
 }
+
+filled <- lapply(file_split, fill_in, length=n_samples + 1)
+
+#samples_distance_matrix <- matrix(unlist(filled), nrow=n_samples, byrow=TRUE)
+samples_distance_matrix <- do.call(rbind, filled)
+
+samples <- samples_distance_matrix[,1]
+
+dist_matrix <- samples_distance_matrix[,-1]
+dist_matrix <- matrix(as.numeric(dist_matrix), nrow=n_samples)
+dist_matrix <- dist_matrix + t(dist_matrix)
+
