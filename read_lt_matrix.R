7f4a0efbd4de39a2f9f4fd8684ea1328fe7a7f71
Pat Schloss <pschloss@umich.edu>
2021-12-10 09:08:07 -0500

    Pull out the number of samples in distance matrix

--- read_lt_matrix.R
+++ read_lt_matrix.R
@@ -2,3 +2,6 @@ distances <- scan("mice_simple.braycurtis.dist",
                   what=character(),
                   quiet=TRUE,
                   sep="\t")
+
+n_samples <- as.numeric(distances[1])
+distances <- distances[-1]
