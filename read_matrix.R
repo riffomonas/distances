f95509595c134b11e3cef89c3e151007462f7ae8
Pat Schloss <pschloss@umich.edu>
2022-01-07 09:32:37 -0500

    Convert code to a function

--- read_matrix.R
+++ read_matrix.R
@@ -1,29 +1,34 @@
-file <- scan("mice_simple.braycurtis.dist",
-                  what=character(),
-                  quiet=TRUE,
-                  sep="\n")
+read_matrix <- function(file_name){
+  
+  file <- scan(file_name,
+               what=character(),
+               quiet=TRUE,
+               sep="\n")
+  
+  n_samples <- as.numeric(file[1])
+  file <- file[-1]
+  
+  file_split <- strsplit(file, "\t")
+  
+  fill_in <- function(x, length){
+    c(x, rep("0", length - length(x)))
+  }
+  
+  filled <- lapply(file_split, fill_in, length=n_samples + 1)
+  
+  samples_distance_matrix <- do.call(rbind, filled)
+  
+  samples <- samples_distance_matrix[,1]
+  
+  dist_matrix <- samples_distance_matrix[,-1]
+  dist_matrix <- matrix(as.numeric(dist_matrix), nrow=n_samples)
+  
+  if(sum(dist_matrix[upper.tri(dist_matrix)]) == 0){
+    dist_matrix <- dist_matrix+t(dist_matrix)
+  }
+  
+  rownames(dist_matrix) <- samples
+  colnames(dist_matrix) <- samples
 
-n_samples <- as.numeric(file[1])
-file <- file[-1]
-
-file_split <- strsplit(file, "\t")
-
-fill_in <- function(x, length){
-  c(x, rep("0", length - length(x)))
+  return(dist_matrix)
 }
-
-filled <- lapply(file_split, fill_in, length=n_samples + 1)
-
-samples_distance_matrix <- do.call(rbind, filled)
-
-samples <- samples_distance_matrix[,1]
-
-dist_matrix <- samples_distance_matrix[,-1]
-dist_matrix <- matrix(as.numeric(dist_matrix), nrow=n_samples)
-
-if(sum(dist_matrix[upper.tri(dist_matrix)]) == 0){
-  dist_matrix <- dist_matrix+t(dist_matrix)
-}
-
-rownames(dist_matrix) <- samples
-colnames(dist_matrix) <- samples
