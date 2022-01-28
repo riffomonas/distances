6e12a7cf418c61509bbdfea79af830fa25dc369d
Pat Schloss <pschloss@umich.edu>
2022-01-28 16:58:21 -0500

    More sophisticated search pattern

--- code/analysis.R
+++ code/analysis.R
@@ -7,6 +7,9 @@ dist_tbl <- as_tibble(dist_matrix, rownames="samples")
 
 sample_lookup <- dist_tbl %>% 
   select(samples) %>%
-  mutate(animal = str_replace(samples, "D\\d*", ""),
-         sex = str_replace(samples, "\\d*D\\d*", ""),
-         day = as.numeric(str_replace(samples, "\\w\\d*D", "")))
+  mutate(delimited = str_replace(samples,
+                                 "^(([FM])\\d+)D(\\d+)$",
+                                 "\\2-\\1-\\3")) %>%
+  separate(col=delimited,
+           into=c("sex", "animal", "day"), sep="-",
+           convert=TRUE)
