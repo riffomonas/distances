310ff4a1141e0a460bc6228bda0585cb87d4db11
Pat Schloss <pschloss@umich.edu>
2022-01-28 16:25:58 -0500

    Create sample_lookup tibble

--- code/analysis.R
+++ code/analysis.R
@@ -4,3 +4,9 @@ source("code/read_matrix.R")
 dist_matrix <- read_matrix("data/mice.braycurtis.dist")
 
 dist_tbl <- as_tibble(dist_matrix, rownames="samples")
+
+sample_lookup <- dist_tbl %>% 
+  select(samples) %>%
+  mutate(animal = str_replace(samples, "D\\d*", ""),
+         sex = str_replace(samples, "\\d*D\\d*", ""),
+         day = as.numeric(str_replace(samples, "\\w\\d*D", "")))
