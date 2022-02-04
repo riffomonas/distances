e14ff12ef551aec7bfd74323551d2726cabccd8d
Pat Schloss <pschloss@umich.edu>
2022-02-04 13:09:48 -0500

    Filter distance matrix

--- code/analysis.R
+++ code/analysis.R
@@ -13,3 +13,15 @@ sample_lookup <- dist_tbl %>%
   separate(col=delimited,
            into=c("sex", "animal", "day"), sep="-",
            convert=TRUE)
+
+days_wanted <- c(0:9, 141:150)
+
+dist_matrix <- dist_tbl %>%
+  pivot_longer(cols=-samples, names_to="b", values_to="distances") %>%
+  inner_join(., sample_lookup, by="samples") %>%
+  inner_join(., sample_lookup, by=c("b" = "samples")) %>%
+  filter(day.x %in% days_wanted & day.y %in% days_wanted) %>%
+  select(samples, b, distances) %>%
+  pivot_wider(names_from="b", values_from="distances") %>%
+  select(-samples) %>%
+  as.dist()
