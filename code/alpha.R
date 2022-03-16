48ba60df47e76afb9ae34b275a07dd4d730f3cfa
Pat Schloss <pschloss@umich.edu>
2022-03-16 14:44:04 -0400

    Write our own alpha diversity indices and use vegan's

--- code/alpha.R
+++ code/alpha.R
@@ -1,3 +1,4 @@
+library(vegan)
 library(tidyverse)
 
 set.seed(19760620)
@@ -23,3 +24,55 @@ rand <- shared %>%
   uncount(value) %>%
   mutate(name = sample(name)) %>%
   count(Group, name, name="value")
+
+richness <- function(x){
+  
+  # r <- sum(x > 0)
+  # return(r)
+  
+  sum(x>0)
+}
+
+shannon <- function(x){
+  
+  rabund <- x[x>0]/sum(x)
+  -sum(rabund * log(rabund))
+  
+}
+
+simpson <- function(x){
+  
+  n <- sum(x)
+  
+  # sum(x * (x-1) / (n * (n-1)))
+  1 - sum((x/n)^2)
+}
+
+rand %>%
+  group_by(Group) %>%
+  summarize(sobs = richness(value),
+            shannon = shannon(value),
+            simpson = simpson(value),
+            invsimpson = 1/simpson,
+            n = sum(value)) %>%
+  pivot_longer(cols=c(sobs, shannon, invsimpson, simpson),
+               names_to="metric") %>%
+  ggplot(aes(x=n, y=value)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~metric, nrow=4, scales="free_y")
+
+
+rand %>%
+  group_by(Group) %>%
+  summarize(sobs = specnumber(value),
+            shannon = diversity(value, index="shannon"),
+            simpson = diversity(value, index="simpson"),
+            invsimpson = 1/simpson,
+            n = sum(value)) %>%
+  pivot_longer(cols=c(sobs, shannon, invsimpson, simpson),
+               names_to="metric") %>%
+  ggplot(aes(x=n, y=value)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~metric, nrow=4, scales="free_y")
