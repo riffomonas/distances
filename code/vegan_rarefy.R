2a2da903d1216a50d61ade7837d9e60e4310fd24
Pat Schloss <pschloss@umich.edu>
2022-03-25 12:47:35 -0400

    Create script for vegan rarefy episode

---
+++ code/vegan_rarefy.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(vegan)
+
+days_wanted <- c(0:9, 141:150)
+
+shared <- read_tsv("data/mice.shared") %>%
+  select(Group, starts_with("Otu")) %>%
+  mutate(day = str_replace(Group, ".*D", "")) %>%
+  filter(day %in% days_wanted) %>%
+  select(-day) %>%
+  pivot_longer(-Group) %>%
+  group_by(Group) %>%
+  mutate(total = sum(value)) %>%
+  filter(total > 1800) %>%
+  group_by(name) %>%
+  mutate(total = sum(value)) %>%
+  filter(total != 0) %>%
+  ungroup() %>%
+  select(-total)
+
+
+my_rarefy <- function(x, sample){
+  
+  x <- x[x>0]
+  sum(1-exp(lchoose(sum(x) - x, sample) - lchoose(sum(x), sample)))
+  
+}
