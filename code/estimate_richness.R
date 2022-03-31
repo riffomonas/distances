057459b2e776a324c9403500353b9eaa7558ec9e
Pat Schloss <pschloss@umich.edu>
2022-03-31 12:08:52 -0400

    Estimated richness using breakaway and chao1

--- code/estimate_richness.R
+++ code/estimate_richness.R
@@ -1,4 +1,5 @@
 library(vegan)
+library(breakaway)
 library(tidyverse)
 
 set.seed(19760620)
@@ -24,3 +25,42 @@ rand <- shared %>%
   uncount(value) %>%
   mutate(name = sample(name)) %>%
   count(Group, name, name="value")
+
+get_breakaway <- function(x){
+  
+  ba <- breakaway(x)
+  tibble(est= ba$estimate,
+         lci=ba$interval[1], uci=ba$interval[2],
+         model=ba$model)
+  
+}
+
+get_chao <- function(x){
+  
+  sobs <- sum(x$n)
+  sing <- x[x$value == 1, "n"] %>% pull(n)
+  doub <- x[x$value == 2, "n"] %>% pull(n)
+  
+  sobs + sing^2 / (2*doub)
+  
+}
+
+b_analysis <- rand %>%
+  count(Group, value) %>%
+  nest(data = -Group) %>%
+  mutate(n_seqs = map_dbl(data, ~sum(.x$value * .x$n)),
+         sobs = map_dbl(data, ~sum(.x$n)),
+         rare = map_dbl(data, ~rarefy(rep(.x$value, .x$n), sample=1828)),
+         chao = map_dbl(data, ~get_chao(.x)),
+         ba = map(data, ~get_breakaway(.x))) %>%
+  select(-data) %>%
+  unnest(ba)
+
+
+b_analysis %>%
+  select(Group, n_seqs, sobs, chao, rare, est) %>%
+  pivot_longer(-c(Group, n_seqs)) %>%
+  ggplot(aes(x=n_seqs, y=value, color=name)) +
+  geom_point() +
+  geom_smooth() +
+  coord_cartesian(ylim=c(0, 2000))
