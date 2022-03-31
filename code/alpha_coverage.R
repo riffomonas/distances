34387e1f203317d5071244728fe5ffe292688a3e
Pat Schloss <pschloss@umich.edu>
2022-03-31 14:32:32 -0400

    Coverage episode starter script

---
+++ code/alpha_coverage.R
@@ -0,0 +1,26 @@
+library(vegan)
+library(tidyverse)
+
+set.seed(19760620)
+
+days_wanted <- c(0:9, 141:150)
+
+shared <- read_tsv("data/mice.shared") %>%
+  select(Group, starts_with("Otu")) %>%
+  mutate(day = str_replace(Group, ".*D", "")) %>%
+#  filter(day %in% days_wanted) %>%
+  select(-day) %>%
+  pivot_longer(-Group) %>%
+  group_by(Group) %>%
+  mutate(total = sum(value)) %>%
+  # filter(total > 1800) %>%
+  group_by(name) %>%
+  mutate(total = sum(value)) %>%
+  filter(total != 0) %>%
+  ungroup() %>%
+  select(-total)
+
+rand <- shared %>%
+  uncount(value) %>%
+  mutate(name = sample(name)) %>%
+  count(Group, name, name="value")
