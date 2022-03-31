52a70390b74b5d3fd4dd9a214927ec1b1424d64f
Pat Schloss <pschloss@umich.edu>
2022-03-31 14:32:46 -0400

    Calculate good's coverage and visualize read depths

--- code/alpha_coverage.R
+++ code/alpha_coverage.R
@@ -24,3 +24,42 @@ rand <- shared %>%
   uncount(value) %>%
   mutate(name = sample(name)) %>%
   count(Group, name, name="value")
+
+sampling_coverage <- shared %>% 
+  group_by(Group) %>%
+  summarize(n_seqs = sum(value))
+
+sampling_coverage %>%
+  ggplot(aes(x=n_seqs)) +
+  geom_histogram(binwidth=500) +
+  coord_cartesian(xlim=c(0, 5000))
+
+sampling_coverage %>%
+  ggplot(aes(x=1, y=n_seqs)) +
+  geom_jitter() +
+  scale_y_log10()
+
+sampling_coverage %>%
+  arrange(n_seqs) %>%
+  ggplot(aes(x=1:nrow(.), y=n_seqs))+
+  geom_line() +
+  coord_cartesian(xlim=c(0, 50), ylim=c(0, 5000))
+
+sampling_coverage %>%
+  arrange(n_seqs) %>%
+  print(n=20)
+
+
+coverage_stats <- shared %>%
+  group_by(Group) %>%
+  summarize(n_seqs = sum(value),
+            n_sings = sum(value == 1),
+            goods = 100*(1 - n_sings / n_seqs)) %>%
+  filter(n_seqs > 1800)
+
+coverage_stats %>%
+  ggplot(aes(x=n_seqs, y=goods)) +
+  geom_point()
+
+coverage_stats %>% 
+  arrange(goods)
